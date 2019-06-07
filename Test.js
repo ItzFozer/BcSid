4a5d98a6e49a7badfabc8e91672c0b1af1c68404
ItzFozer <50673642+ItzFozer@users.noreply.github.com>
2019-06-07 22:17:38 +0000

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,94 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "-"
+var adminprefix = '-'
+
+
+//bc
+
+client.on("message", message => {
+    if (message.content.startsWith("-1bc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  **__المستلمين__** `);
+  message.delete();
+  };
+  });
+
+
+//bc online
+
+
+  var prefix = "-";
+
+  client.on("message", message => {
+  
+              if (message.content.startsWith(prefix + "-2bc")) {
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+   m.send(`${argresult}\n ${m}`);
+  })
+   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
+   message.delete(); 
+  };     
+  });
+
+client.on('message', message => {
+    var  user = message.mentions.users.first() || message.author;
+if (message.content.startsWith("$avatar")) {
+message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
+}
+});
+
+client.on('ready',  () => {
+    console.log('تم تشغيل :Broadcast  ');
+    console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
+  });
+
+  client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('-3bc')){
+if(!message.author.id === '476185102922285066') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+  client.on('message', msg => {
+    if(msg.content === '-help')
+    msg.reply('Check Your DM :white_check_mark:')
+  });
+  
+  
+  client.on("message", message => {
+    if (message.content === "-help") {
+     const embed = new Discord.RichEmbed() 
+         .setColor("RANDOM")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription(`**Help|هيلب
+
+       -1bc | لأرسال برود كاست للكل
+
+       -2bc  |  لأرسال برود كاست للأونلاين
+
+       -3bc | برودكاست عادي
+
+       ** `)
+   message.author.sendEmbed(embed)
+   
+   }
+   });
+
+
+client.login(process.env.TOKEN);
